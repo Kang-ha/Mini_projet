37275995808919f4a53801aa8b03cb2751e957b9
DUJARDIN Killian <dujardin.k@stjolorient.fr>
2025-01-23 16:15:47 +0100

    Mise à jour de la fonction creer_socket - #5

--- TCP/tcp.c
+++ TCP/tcp.c
@@ -10,7 +10,7 @@
 
 /* Créer une socket */
 int sock;
-sock = creer_socket(AF_INET,SOCK_DGRAM,0) {
+sock = int creer_socket(char* adresseIP, int port,0) {
 	if (sock == -1) {
 		printf("Echec de création socket\n");
 		traiter_erreur(__FUNCTION__);
