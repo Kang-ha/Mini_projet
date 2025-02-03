0203ff1e4b33944b921c23670f1e82aa0d9f1377
boutin <boutin.l@stjolorient.fr>
2025-02-03 14:03:28 +0100

    Modification du code

--- UDP/udp.c
+++ UDP/udp.c
@@ -10,26 +10,39 @@
 
 /* Créer une socket */
 void creer_socket(char* adresseIP, int port, SOCK* sock) {
+	sock->sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	traiter_erreur(__FUNCTION__);
+
+	// adresse et port reutilisable
+	int optval = 1;
+    	setsockopt(sock->sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+	traiter_erreur(__FUNCTION__);
+
 	
 	/*A COMPLETER*/
-	traiter_erreur(__FUNCTION__);
-	/*A COMPLETER*/
+
+	if (strcmp(adresseIP, "") != 0)
+		sock->adresse.sin_addr.s_addr = inet_addr(adresseIP);
+	else 
+		sock->adresse.sin_addr.s_addr = INADDR_ANY;
 }
 
 /* Attacher une socket */
 void attacher_socket(SOCK* sock) {
+	bind(sock, (struct sockaddr*) &addr_srv, sizeof(addr_srv))
+	
 	/*A COMPLETER*/
 	traiter_erreur(__FUNCTION__);
 }
 
-/*Initialiser la structure adresse client */
+/*Initialiser la structure adresse */
 void init_addr(SOCK* sock) {
-	/*A COMPLETER*/
+	memset(&sock->adresse, 0, sizeof(sock->adresse));
 }
 
 /* Dimensionner la file d'attente d'une socket */
 void dimensionner_file_attente_socket(int taille, SOCK* sock) {
-	/*A COMPLETER*/
+	listen(sock->sockfd, taille);
 	traiter_erreur(__FUNCTION__);
 }
 
@@ -46,6 +59,6 @@ void envoyer_message(SOCK* dst, char * message) {
 }
 /* Fermer la connexion */
 void fermer_connexion(SOCK* sock) {
-	/*A COMPLETER*/
+	close(sock->sockfd);
 	traiter_erreur(__FUNCTION__);
 }
